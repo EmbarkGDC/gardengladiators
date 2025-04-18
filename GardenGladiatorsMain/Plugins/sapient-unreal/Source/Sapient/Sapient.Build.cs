ce7d31c5150685187a790b6752ad8cfc6a1f88e9
Stevie Larsen <maxpendragon@gmail.com>
2025-04-17 17:16:26 -0700

    Updated Sapient

--- GardenGladiatorsMain/Plugins/sapient-unreal/Source/Sapient/Sapient.Build.cs
+++ GardenGladiatorsMain/Plugins/sapient-unreal/Source/Sapient/Sapient.Build.cs
@@ -1,99 +1,132 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 // Copyright © 2024 - 2025 Sapient Technology, Inc.
 
-using UnrealBuildTool;
 using System.IO;
+using UnrealBuildTool;
 
 public class Sapient : ModuleRules
 {
-    public Sapient(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+	public Sapient(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		// Binary distribution settings
 
 		bUsePrecompiled = true;
-        bPrecompile = true;
-        PrecompileForTargets = PrecompileTargetsType.Any;
-        
-        // Optimization settings
+		bPrecompile = true;
+		PrecompileForTargets = PrecompileTargetsType.Any;
+
+		// Optimization settings
 #if UE_5_5_OR_LATER
-        UndefinedIdentifierWarningLevel = WarningLevel.Off;
+		UndefinedIdentifierWarningLevel = WarningLevel.Off;
 #endif
-        OptimizeCode = CodeOptimization.InShippingBuildsOnly;
+		OptimizeCode = CodeOptimization.InShippingBuildsOnly;
 
-        PublicIncludePaths.AddRange(
-            new string[] 
-            {
-                Path.Combine(ModuleDirectory, "Public")
-            }
-        );
-        
-        PrivateIncludePaths.AddRange(
-            new string[] 
-            {
-                Path.Combine(ModuleDirectory, "Private")
-            }
-        );
-
-        // Core dependencies required for both runtime and editor
-        PublicDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "Core",
-                "CoreUObject",
-                "Engine",
-                "InputCore",
-                "Json",
-                "JsonUtilities",
-                "HTTP",
-                "Projects",
-				"LevelEditor"
+		PublicIncludePaths.AddRange(
+			new[]
+			{
+				Path.Combine(ModuleDirectory, "Public")
 			}
-        );
+		);
 
-        // Editor-specific dependencies
-        PrivateDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "UnrealEd",
-                "Slate",
-                "SlateCore",
-                "EditorFramework",
-                "EditorStyle",
-                "ToolMenus",
-                "AssetTools",
-                "BlueprintGraph",
-                "BlueprintEditorLibrary",
-                "ContentBrowser",
-                "WorkspaceMenuStructure",
-                "PythonScriptPlugin",
-                "HotReload",
-                "MainFrame",
-                "AIModule",
-                "GameplayTasks",
-                "Settings"
+		PrivateIncludePaths.AddRange(
+			new[]
+			{
+				Path.Combine(ModuleDirectory, "Private")
 			}
-        );
+		);
 
-        // Additional editor functionality
-        if (Target.bBuildEditor)
-        {
-            PrivateDependencyModuleNames.AddRange(
-                new string[]
-                {
-                    "PropertyEditor",
-                    "DesktopPlatform",
-                    "ApplicationCore"
-                }
-            );
-        }
+		// Core dependencies required for both runtime and editor
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"Json",
+				"JsonUtilities",
+				"HTTP",
+				"Projects",
+				"LevelEditor",
+				"AIModule"
+			}
+		);
 
-        // Binary distribution protection
-        bRequiresImplementModule = true;
-        PublicDefinitions.Add("SAPIENT_API=DLLEXPORT");
-        
-        // Additional compiler settings for editor plugin
-        bEnableExceptions = true;
-    }
+		// Editor-specific dependencies
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"UnrealEd",
+				"Slate",
+				"SlateCore",
+				"EditorFramework",
+				"EditorStyle",
+				"ToolMenus",
+				"AssetTools",
+				"BlueprintGraph",
+				"BlueprintEditorLibrary",
+				"ContentBrowser",
+				"WorkspaceMenuStructure",
+				"PythonScriptPlugin",
+				"HotReload",
+				"MainFrame",
+				"AIModule",
+				"GameplayTasks",
+				"Settings",
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"UnrealEd",
+				"LevelEditor",
+				"Kismet",
+				"KismetCompiler",
+				"BlueprintGraph",
+				"GraphEditor",
+				"PropertyEditor",
+				"EditorStyle",
+				"AssetRegistry",
+				"Json",
+				"JsonUtilities",
+				"EditorSubsystem",
+				"AIModule",
+				"EditorScriptingUtilities",
+				"BehaviorTreeEditor",
+				"AIGraph"
+			}
+		);
+
+		// Additional editor functionality
+		if (Target.bBuildEditor)
+			PrivateDependencyModuleNames.AddRange(
+				new[]
+				{
+					"PropertyEditor",
+					"DesktopPlatform",
+					"ApplicationCore"
+				}
+			);
+
+		// Binary distribution protection
+		bRequiresImplementModule = true;
+		PublicDefinitions.Add("SAPIENT_API=DLLEXPORT");
+
+
+		/**
+		 * @brief Controls shipping build logging behavior
+		 *
+		 * When set to 1, development logging is disabled and only critical logs remain
+		 * When set to 0, all logging is enabled for development purposes
+		 *
+		 * @note Modify this value before building for different configurations:
+		 * - Development/Debug: Set to 0
+		 * - Shipping/Release: Set to 1
+		 */
+		PublicDefinitions.Add("SAPIENT_UE_BUILD_SHIPPING=1");
+
+		// Additional compiler settings for editor plugin
+		bEnableExceptions = true;
+	}
 }
